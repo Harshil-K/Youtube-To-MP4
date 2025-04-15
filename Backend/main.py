646c46255b99069be16bbf21c0da5af90c351fe8
hashi11002 <harshilkumar12@gmail.com>
2025-04-15 01:28:33 -0400

    backend changes

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
-from downloadAPI import downloadVideo
+from Backend.downloadAPI import downloadVideo
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
@@ -29,8 +29,8 @@ async def download(data: URLRequest):
     filename = downloadVideo(data.url)
     return FileResponse(filename, media_type="video/mp4", filename="video.mp4")
 
-app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
+app.mount("/", StaticFiles(directory="Frontend", html=True), name="Frontend")
 
 @app.get("/")
 async def serve_home():
-    return FileResponse("frontend/index.html")
+    return FileResponse("Frontend/index.html")
